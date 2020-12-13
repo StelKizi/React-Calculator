c949c04fc73891c3a65a811a743649118ada6ec5
Stelkizi <stelkiziridou@gmail.com>
2020-12-13 22:51:27 +0100

    added some of the logic

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,128 @@
-import React from 'react';
-import Buttons from './Buttons.js';
+import React, { useState } from 'react';
+import Button from './Button';
 import '../styles/App.css';
 
 function App() {
+	const [input, setInput] = useState('0');
+	const [previousValue, setPreviousValue] = useState(null);
+	const [operator, setOperator] = useState(null);
+
+	const handleClick = (content /* , type2 */) => {
+		const number = parseFloat(input);
+
+		setInput('0');
+		if (content === 'C') {
+			setPreviousValue(null);
+			return;
+		}
+
+		if (content === '+/−') {
+			setInput(number * -1).toString();
+			return;
+		}
+
+		if (content === '%') {
+			setInput((number / 100).toString());
+			/* setPreviousValue(null); */
+			setOperator(null);
+			return;
+		}
+
+		if (content === '+') {
+			setPreviousValue(parseFloat(input));
+			setInput('0');
+			setOperator('+');
+			return;
+		}
+
+		if (content === '−') {
+			setPreviousValue(parseFloat(input));
+			setInput('0');
+			setOperator('−');
+			return;
+		}
+
+		if (content === '×') {
+			setPreviousValue(parseFloat(input));
+			setInput('0');
+			setOperator('×');
+			return;
+		}
+
+		if (content === '÷') {
+			setPreviousValue(parseFloat(input));
+			setInput('0');
+			setOperator('÷');
+			return;
+		}
+
+		if (content === '=') {
+			setPreviousValue(null);
+			setOperator(null);
+			return;
+		}
+
+		setInput(parseFloat(number + content).toString());
+	};
+
 	return (
 		<div className="App">
-			<div className="display">display</div>
+			<div className="display">{input}</div>
+
 			<div className="buttons">
-				<Buttons />
+				<Button
+					handleClick={handleClick}
+					content="C"
+					type="functionOrOperator"
+				/>
+				<Button
+					handleClick={handleClick}
+					content="⌫"
+					type="functionOrOperator"
+				/>
+				<Button
+					handleClick={handleClick}
+					content="%"
+					type="functionOrOperator"
+					/* type2="operator" */
+				/>
+				<Button
+					handleClick={handleClick}
+					content="÷"
+					type="functionOrOperator"
+					/* type2="operator" */
+				/>
+				<Button handleClick={handleClick} content="7" />
+				<Button handleClick={handleClick} content="8" />
+				<Button handleClick={handleClick} content="9" />
+				<Button
+					handleClick={handleClick}
+					content="×"
+					type="functionOrOperator"
+					/* type2="operator" */
+				/>
+				<Button handleClick={handleClick} content="4" />
+				<Button handleClick={handleClick} content="5" />
+				<Button handleClick={handleClick} content="6" />
+				<Button
+					handleClick={handleClick}
+					content="−"
+					type="functionOrOperator"
+					/* type2="operator" */
+				/>
+				<Button handleClick={handleClick} content="1" />
+				<Button handleClick={handleClick} content="2" />
+				<Button handleClick={handleClick} content="3" />
+				<Button
+					handleClick={handleClick}
+					content="+"
+					type="functionOrOperator"
+					/* type2="operator" */
+				/>
+				<Button handleClick={handleClick} content="+/−" />
+				<Button handleClick={handleClick} content="0" />
+				<Button handleClick={handleClick} content="." />
+				<Button handleClick={handleClick} content="=" />
 			</div>
 		</div>
 	);
--- src/components/Button.js
+++ src/components/Button.js
@@ -1,6 +1,13 @@
 import React from 'react';
 import '../styles/Button.css';
 
-export default function Button({ content }) {
-	return <div className={'button'}>{content}</div>;
+export default function Button({ content, type, handleClick }) {
+	return (
+		<div
+			className={`button ${content === '=' ? 'equals' : ''}${type || ''}`}
+			onClick={() => handleClick(content)}
+		>
+			{content}
+		</div>
+	);
 }
